12aa1f5276cdf93875955de0df64d77faa357f0c
RobertStowers <47610421+RobertStowers@users.noreply.github.com>
2019-09-15 10:54:48 -0400

    Refactor bullet update code

--- game_functions.py
+++ game_functions.py
@@ -35,6 +35,17 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
+def update_bullets(bullets):
+    """Update position of bullets and get rid of old bullets."""
+    # Update bullet positions.
+    bullets.update()
+
+    # Get rid of bullets that have disappeared
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+
 def update_screen(ai_settings, screen, ship, bullets):
     """Update images on the screen and flip to the new screen."""
 
