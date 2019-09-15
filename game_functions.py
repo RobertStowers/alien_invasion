98c52172a2f0dfc5f03c356ba0055da15e67e8d7
RobertStowers <47610421+RobertStowers@users.noreply.github.com>
2019-09-15 11:04:15 -0400

    Refactor fire_bullet function

--- game_functions.py
+++ game_functions.py
@@ -10,10 +10,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Create a new bullet and add it to the bullets group.
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 
 def check_keyup_events(event, ship):
@@ -33,6 +30,14 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Fire a bullet if limit reached yet."""
+    # Create a new bullet and add it to the bullets group.
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def update_bullets(bullets):
