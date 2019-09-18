331b77ffedcfa9fb8802d322169f62c5e45164f8
Robert Stowers <robert.stowers@gmail.com>
2019-09-18 11:33:01 -0400

    Added high score to screen

--- scoreboard.py
+++ scoreboard.py
@@ -15,8 +15,9 @@ class Scoreboard():
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
 
-        # Prepare the initial score image.
+        # Prepare the initial score images.
         self.prep_score()
+        self.prep_high_score()
 
 
     def prep_score(self):
@@ -34,7 +35,21 @@ class Scoreboard():
         self.score_rect.top = 20
 
 
+    def prep_high_score(self):
+        """Turn the high score into a rendered image."""
+        high_score = int(round(self.stats.high_score, -1))
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.ai_settings.bg_color)
+
+        # Center the high score at the top of the screen.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+
     def show_score(self):
         """Draw score to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+
 
